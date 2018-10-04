1cf35e021d8d9cec2797ee2845cc9c81ead196b8
Anthony Ishkan <anthony.ishkan@gmail.com>
2018-10-04 19:36:18 +1000

    Add rect array Flatten extension method and test.

--- Minesweeper RC Test/ExtensionsTest.cs
+++ Minesweeper RC Test/ExtensionsTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Minesweeper_RC.Extensions;
+using System;
 using System.Drawing;
 
 namespace Minesweeper_RC_Test
@@ -23,6 +24,15 @@ namespace Minesweeper_RC_Test
             int Xfactor = 2, Yfactor = 3;
             var expected = new Size(size.Width * Xfactor, size.Height * Yfactor);
             Assert.AreEqual<Size>(expected, size.MultiplyBy(Xfactor, Yfactor));
+        }
+
+        [TestMethod]
+        public void TestFlattenRectArray()
+        {
+            var toTest = new int[3, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            var expected = new int[9] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var actual = toTest.Flatten();
+            CollectionAssert.AreEqual(expected, actual);
         }
     }
 }
--- Minesweeper RC/Extensions.cs
+++ Minesweeper RC/Extensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 
 namespace Minesweeper_RC.Extensions
 {
@@ -25,6 +26,23 @@ namespace Minesweeper_RC.Extensions
         public static Size MultiplyBy(this Size size, double XFactor, double YFactor)
         {
             return new Size((int)(size.Width * XFactor), (int)(size.Height * YFactor));
+        }
+
+        /// <summary>
+        /// Flattens a 2D rectangular array to a 1D array. Elements are shallow-copied into
+        /// the new array in this order - (0:0, 0:1 .. 0:X, 1:0, 1:1, ...)
+        /// </summary>
+        /// <typeparam name="T">The type of the array elements.</typeparam>
+        /// <param name="values">The array to flatten.</param>
+        /// <returns>The flattened array.</returns>
+        public static T[] Flatten<T>(this T[,] values)
+        {
+            var flat = new T[values.Length];
+            var rowSize = values.GetLength(0);
+            for (var y = 0; y < values.GetLength(1); y++)
+                for (var x = 0; x < values.GetLength(0); x++)
+                    flat[(y * rowSize) + x] = values[y, x];
+            return flat;
         }
     }
 }
