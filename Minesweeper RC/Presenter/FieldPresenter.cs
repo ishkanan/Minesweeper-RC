84363b45fd758e6193875c7c382986d68abcd30d
Anthony Ishkan <anthony.ishkan@gmail.com>
2018-10-04 20:48:49 +1000

    FieldPresenter possibly complete, requires tests.

--- Minesweeper RC/Presenter/FieldPresenter.cs
+++ Minesweeper RC/Presenter/FieldPresenter.cs
@@ -12,21 +12,32 @@ namespace Minesweeper_RC.Presenter
     public class FieldPresenter
     {
         private IFieldView _fieldView;
-        private IField _field;
+        private IGame _game;
 
-        public FieldPresenter(IFieldView fieldView, IField field)
+        public FieldPresenter(IFieldView fieldView, IGame game)
         {
             _fieldView = fieldView;
-            _field = field;
+            _game = game;
 
             _fieldView.CellClick += OnCellClick;
-            _fieldView.RenderField(_field);
+            _fieldView.RenderField(_game.Minefield);
         }
 
         private void OnCellClick(Cell cell, MouseButtons buttons)
         {
-            // reveal cell via IGame
-            _fieldView.RenderField(_field);
+            try
+            {
+                _game.Reveal(cell.Location);
+            }
+            catch (InvalidOperationException)
+            {
+                // ignore these errors as no impact to user
+                return;
+            }
+
+            // no more input is accepted if game is no longer running
+            _fieldView.AllowInput = _game.State == GameState.Running;
+            _fieldView.RenderField(_game.Minefield);
         }
     }
 }
