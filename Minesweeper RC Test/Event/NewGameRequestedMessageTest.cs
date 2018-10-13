c83d0c1767f4d246dc64e0c82759947eb9bf2bea
Anthony Ishkan <anthony.ishkan@gmail.com>
2018-10-13 16:02:15 +1100

    Add NewGameRequested message plus tests.

---
+++ Minesweeper RC Test/Event/NewGameRequestedMessageTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Minesweeper_RC.Event;
+using Minesweeper_RC.Model;
+
+namespace Minesweeper_RC_Test.Event
+{
+    [TestClass]
+    public class NewGameRequestedMessageTest
+    {
+        [TestMethod]
+        public void TestPropertiesAfterInstantiate()
+        {
+            var m = new NewGameRequestedMessage(null);
+            Assert.IsNull(m.Game);
+            var game = new Game(SkillLevel.Beginner);
+            m = new NewGameRequestedMessage(game);
+            Assert.AreEqual<IGame>(game, m.Game);
+        }
+    }
+}
---
+++ Minesweeper RC/Event/NewGameRequestedMessage.cs
@@ -0,0 +1,15 @@
+﻿using Minesweeper_RC.Model;
+
+namespace Minesweeper_RC.Event
+{
+    public class NewGameRequestedMessage : IMessage
+    {
+        public IGame Game { get; private set; }
+
+        public NewGameRequestedMessage(IGame game)
+        {
+            // used by MainForm (if game exists, create new with same skill level)
+            Game = game;
+        }
+    }
+}
